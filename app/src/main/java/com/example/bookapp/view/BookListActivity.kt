20739f36202bbb93af2b75b79305f3b4e0f9d7c3
meiqi <meiqi.xiao@thoughtworks.com>
2024-04-30 22:38:53 +0800

    init project

---
+++ app/src/main/java/com/example/bookapp/view/BookListActivity.kt
@@ -0,0 +1,12 @@
+package com.example.bookapp.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.bookapp.R
+
+class BookListActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_book_list)
+    }
+}
